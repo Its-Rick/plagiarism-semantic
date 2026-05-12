3c208ffa826b784b7696b5224a91e2102c69a362
Its-Rick <rdeb092004@gmail.com>
2026-05-12 10:26:31 +0530

    render url

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", "http://localhost:3000",
         "http://localhost:8000", "http://127.0.0.1:8000",
+        "https://plagiarism-semantic.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
